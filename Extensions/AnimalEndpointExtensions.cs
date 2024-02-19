baa86ecf35cebbd16dc998d89ab0a72aaf671bef
Tobias Lindberg <uet.lindberg@outlook.com>
2024-02-19 12:53:58 +0100

    -

---
+++ Extensions/AnimalEndpointExtensions.cs
@@ -0,0 +1,37 @@
+﻿namespace LiveDemo.Extensions;
+
+public static class AnimalEndpointExtensions
+{
+    public static WebApplication MapAnimalEndpoints(this WebApplication app)
+    {
+        //CREATE
+        app.MapPost("/", (AnimalService service, Animal animal) =>
+        {
+            service.Animals.Add(animal);
+        });
+
+        //READ
+        app.MapGet("/", (AnimalService service) => service.Animals);
+
+        //UPDATE
+        app.MapPut("/{id}", (AnimalService service, int id, Animal animal) =>
+        {
+            //service.Animals[id] = animal;
+            service.Animals.Replace(animal);
+        });
+
+        app.MapPatch("/{id}", (AnimalService service, string type, int id) =>
+        {
+            service.Animals[id].Type = type;
+        });
+
+        //DELETE
+        app.MapDelete("/{id}", (AnimalService service, int id) =>
+        {
+            //service.Animals.RemoveAt(id);
+            service.RemoveAnimal();
+        });
+
+        return app;
+    }
+}
