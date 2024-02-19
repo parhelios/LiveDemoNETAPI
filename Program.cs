1fea510423af14e7a7b3a9b188e2eb86cf24205e
Tobias Lindberg <uet.lindberg@outlook.com>
2024-02-19 15:02:44 +0100

    migration fungerar icke än

--- Program.cs
+++ Program.cs
@@ -1,11 +1,32 @@
+using DataAccess;
 using LiveDemo.Extensions;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("PeopleDb");
+
+builder.Services.AddDbContext<PeopleContext>(
+    options =>
+    {
+        options.UseSqlServer(connectionString);
+    });
 
 builder.Services.AddSingleton<AnimalService>();
 
 var app = builder.Build();
 
 app.MapAnimalEndpoints();
+
+app.MapGet("api/people", () => (PeopleContext context) =>
+{
+    return context.People.ToListAsync();
+});
+
+app.MapPost("api/people", (PeopleContext context, Person person) =>
+{
+    context.Add(person);
+    context.SaveChanges();
+});
 
 app.Run();
