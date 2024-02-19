c333f0c5a5d61b74f45c18dc6e24bac7defe4224
Tobias Lindberg <uet.lindberg@outlook.com>
2024-02-19 13:01:13 +0100

    brutit ut endpoint-metoder

--- Extensions/AnimalEndpointExtensions.cs
+++ Extensions/AnimalEndpointExtensions.cs
@@ -1,37 +1,50 @@
-﻿namespace LiveDemo.Extensions;
+﻿
+namespace LiveDemo.Extensions;
 
 public static class AnimalEndpointExtensions
 {
     public static WebApplication MapAnimalEndpoints(this WebApplication app)
     {
         //CREATE
-        app.MapPost("/", (AnimalService service, Animal animal) =>
-        {
-            service.Animals.Add(animal);
-        });
+        app.MapPost("/", AddAnimal);
 
         //READ
-        app.MapGet("/", (AnimalService service) => service.Animals);
+        app.MapGet("/", GetAllAnimals);
 
         //UPDATE
-        app.MapPut("/{id}", (AnimalService service, int id, Animal animal) =>
-        {
-            //service.Animals[id] = animal;
-            service.Animals.Replace(animal);
-        });
-
-        app.MapPatch("/{id}", (AnimalService service, string type, int id) =>
-        {
-            service.Animals[id].Type = type;
-        });
+        app.MapPut("/{id}", ReplaceAnimal);
+        app.MapPatch("/{id}", UpdateAnimal);
 
         //DELETE
-        app.MapDelete("/{id}", (AnimalService service, int id) =>
-        {
-            //service.Animals.RemoveAt(id);
-            service.RemoveAnimal();
-        });
+        app.MapDelete("/{id}", DeleteAnimal);
 
         return app;
+    }
+
+    private static void DeleteAnimal(AnimalService service, int id)
+    {
+        //service.Animals.RemoveAt(id);
+        service.RemoveAnimal();
+    }
+
+    private static void UpdateAnimal(AnimalService service, string type, int id)
+    {
+        service.Animals[id].Type = type;
+    }
+
+    private static void ReplaceAnimal(AnimalService service, int id, Animal animal)
+    {
+        //service.Animals[id] = animal;
+        service.Animals.Replace(animal);
+    }
+
+    private static List<Animal> GetAllAnimals(AnimalService service)
+    {
+        return service.Animals;
+    }
+
+    private static void AddAnimal(AnimalService service, Animal animal)
+    {
+        service.Animals.Add(animal);
     }
 }
